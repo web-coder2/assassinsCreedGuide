ef8de5dd26ee5ee3f70a3f9f8916eb59c355884d
web-coder <yasher18@bk.ru>
2026-05-30 13:32:00 +0300

    вывод таймлайна ивентов

--- client/src/views/VersionsView.vue
+++ client/src/views/VersionsView.vue
@@ -1,5 +1,19 @@
 <template>
     <h3 class="text-light">Таймлайн версий разработки форума</h3>
+
+    <div class="container-fluid">
+        <div class="col-sm-4 offset-sm-8">
+            <div class="timeline">
+                <div v-for="(item, idx) in versionsData" class="timeline-item bg-dark text-light">
+                    <h3>{{ item.versionName }}</h3>
+                    <p>{{ item.versionDescription }}</p>
+                    <span>{{ item.versionDate }}</span>
+                    <h1 v-if="item.versionProde" class="text-warning">{{ item.versionProde }}</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </template>
 
 <script>
@@ -17,7 +31,7 @@
                         col: 'versions/versionsList',
                         params: {}
                     })
-                    this.versionsData = response.data.data
+                    this.versionsData = response.data
                 } catch (e) {
                     console.log(e.message)
                 }
